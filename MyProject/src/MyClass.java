451d6e540254fda9d369b7910256d8d1dc6578ec
Nahid <Nahid@Nahid-HP>
2023-03-30 17:00:02 +0530

    My First Commit

---
+++ MyProject/src/MyClass.java
@@ -0,0 +1,13 @@
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		String hungry = "yes";
+		if(hungry.equalsIgnoreCase("yes"))
+			System.out.println("Eat Something..");
+		else
+			System.out.println("Do Work..");
+
+	}
+
+}
